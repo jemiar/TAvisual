1d0a69b123c128a2a9941da0c25a5179c1be51ff
HoangMinh45 <hhuynh20@uic.edu>
2018-04-02 10:50:33 -0500

    add 2 graphs + edit csv file

--- chart_fall17_spr18.js
+++ chart_fall17_spr18.js
@@ -1,3 +1,24 @@
+var weekNo = [
+	1, 2, 3, 4,
+	5, 6, 7, 8,
+	9, 10, 11, 12,
+	13, 14, 15, 16, 17
+];
+
+var chartF17 = d3.select(".chartFall17"),
+	chartS18 = d3.select(".chartSpr18");
+
+var margin = {top: 20, right: 80, bottom: 80, left: 100},
+	width = chartF17.attr("width") - margin.left - margin.right,
+	height = chartF17.attr("height") - margin.top - margin.bottom;
+
+var chartF17Graphic = chartF17.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	chartS18Graphic = chartS18.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleBand().range([0, width]).padding(0.05),
+	yF17 = d3.scaleLinear().range([height, 0]),
+	yS18 = d3.scaleLinear().range([height, 0]);
+
 d3.queue()
 	.defer(d3.csv, "https://raw.githubusercontent.com/jemiar/TAvisual/master/fall17data.csv", type)
 	.defer(d3.csv, "https://raw.githubusercontent.com/jemiar/TAvisual/master/spring18data.csv", type)
@@ -6,28 +27,313 @@ d3.queue()
 function drawChart(error, fall17data, spring18data) {
 	if(error) { console.log(error); }
 
-	var fall17ByClass = d3.nest().
+	var tempFall17ByClass = d3.nest().
 		key(function(d) { return d.classID; }).
 	    key(function(d) { return d.week; }).
 	    rollup(function(v) { return {
 	    	week: v[0].week,
-	    	total: d3.sum(v, function(d) {return d.labtime + d.classtime + d.grading + d.officehr + d.classprep + d.other; })
+	    	total: d3.sum(v, function(d) { return d.labtime + d.classtime + d.grading + d.officehr + d.classprep + d.other; })
 	    	};
 	    }).
 	    entries(fall17data);
 
-	var spring18ByClass = d3.nest().
+	var fall17ByClass = tempFall17ByClass.map(function(d) { return {
+		key: d.key,
+		val: weekNo.map(function(w) {
+			if(d.values.map(function(k) { return k.value; }).find(function(t) { return t.week == w; }))
+				return d.values.map(function(k) { return k.value; }).find(function(t) { return t.week == w;});
+			else
+				return {week: w, total: 0};
+			})
+		}
+	});
+
+	var tempSpring18ByClass = d3.nest().
     	key(function(d) { return d.classID; }).
     	key(function(d) { return d.week; }).
     	rollup(function(v) { return {
       		week: v[0].week,
-      		total: d3.sum(v, function(d) {return d.labtime + d.classtime + d.grading + d.officehr + d.classprep + d.other; })
+      		total: d3.sum(v, function(d) { return d.labtime + d.classtime + d.grading + d.officehr + d.classprep + d.other; })
       		};
     	}).
     	entries(spring18data);
 
+    var spring18ByClass = tempSpring18ByClass.map(function(d) { return {
+		key: d.key,
+		val: weekNo.map(function(w) {
+			if(d.values.map(function(k) { return k.value; }).find(function(t) { return t.week == w; }))
+				return d.values.map(function(k) { return k.value; }).find(function(t) { return t.week == w;});
+			else
+				return {week: w, total: 0};
+			})
+		}
+	});
+
+	x.domain(fall17ByClass[0].val.map(function(d) { return d.week; }));
+	yF17.domain([
+		d3.min(fall17ByClass[0].val, function(d) { return d.total; }),
+		d3.max(fall17ByClass[0].val, function(d) { return d.total; })
+		]);
+
   	console.log(fall17ByClass);
   	console.log(spring18ByClass);
+
+  	chartF17Graphic.append("g")
+  		.attr("class", "axisX")
+  		.attr("transform", "translate(0," + height + ")")
+  		.call(d3.axisBottom(x).ticks(17))
+  	.append("text")
+  		.attr("transform", "translate(" + width/2 + ", 30)")
+  		.attr("y", 8)
+  		.attr("dy", "0.71em")
+  		.attr("fill", "#000")
+  		.attr("text-align", "right")
+  		.style("font-size", "30px")
+  		.text("Week");
+
+  	chartF17Graphic.append("g")
+  		.attr("class", "axisY")
+  		.attr("id", "axisYFall17")
+  		.call(d3.axisLeft(yF17))
+  	.append("text")
+  		.attr("transform", "rotate(-90)")
+  		.attr("y", 0 - margin.left + 30)
+  		.attr("x", 0 - height / 2 + 100)
+  		.attr("dy", "0.71em")
+  		.attr("fill", "#000")
+  		.style("font-size", "30px")
+  		.text("Working hours");
+
+  	var f17BarChart = chartF17Graphic.selectAll(".barF17")
+  		.data(fall17ByClass[0].val)
+  		.enter().append("rect")
+  		.attr("class", "barF17")
+  		.attr("x", function(d) { return x(d.week); })
+  		.attr("width", x.bandwidth())
+  		.attr("y", function(d) { return yF17(d.total); })
+  		.attr("height", function(d) { return height - yF17(d.total); });
+
+  	var legendFall17Graphic = d3.select(".legendFall17")
+  		.append("g")
+  		.selectAll(".classFall17")
+  		.data(fall17ByClass)
+  		.enter().append("g")
+  		.attr("class", "classFall17");
+
+  	var classFall17Rect = legendFall17Graphic.append("rect")
+  		.attr("class", "classF17Rect")
+  		.attr("id", function(d) { return "classF17Rect" + d.key.replace(/\s/g, ''); })
+  		.attr("x", function(d, i) {
+  			if(i%2 == 0)
+  				return 0;
+  			else
+  				return 75;
+  		})
+  		.attr("y", function(d, i) {
+  			if(i%2 == 0)
+  				return i / 2 * 35;
+  			else
+  				return (i - 1) / 2 * 35;
+  		})
+  		.attr("width", 70)
+  		.attr("height", 30)
+  		.attr("fill", "#d9d9d9")
+  		.on("click", function(d, i) {
+  			classFall17Rect.style("fill", "#d9d9d9");
+  			classFall17Text.style("stroke", "#000");
+  			d3.select(this).style("fill", "steelblue");
+  			classFall17Text._groups[0][i].style.stroke = "#fff";
+
+  			yF17.domain([
+  				d3.min(fall17ByClass[i].val, function(d) { return d.total; }),
+				d3.max(fall17ByClass[i].val, function(d) { return d.total; })
+  				]);
+
+  			d3.select("#axisYFall17")
+  				.transition().duration(500)
+  				.call(d3.axisLeft(yF17));
+
+  			f17BarChart.data(fall17ByClass[i].val).transition().duration(500)
+  				.attr("x", function(d) { return x(d.week); })
+  				.attr("width", x.bandwidth())
+  				.attr("y", function(d) { return yF17(d.total); })
+  				.attr("height", function(d) { return height - yF17(d.total); });
+  		});
+
+  	var classFall17Text = legendFall17Graphic.append("text")
+  		.text(function(d) { return d.key; })
+  		.attr("class", "classF17Text")
+  		.attr("x", function(d, i) {
+  			if(i%2 == 0)
+  				return 10;
+  			else
+  				return 85;
+  		})
+  		.attr("y", function(d, i) {
+  			if(i%2 == 0)
+  				return i / 2 * 35 + 10;
+  			else
+  				return (i - 1) / 2 * 35 + 10;
+  		})
+  		.attr("dy", "0.6em")
+  		.style("font", "lighter 15px sans-serif")
+  		.style("stroke", "#000")
+  		.on("click", function(d, i) {
+  			classFall17Rect.style("fill", "#d9d9d9");
+  			classFall17Text.style("stroke", "#000");
+  			d3.select(this).style("stroke", "#fff");
+  			classFall17Rect._groups[0][i].style.fill = "steelblue";
+
+  			yF17.domain([
+  				d3.min(fall17ByClass[i].val, function(d) { return d.total; }),
+				d3.max(fall17ByClass[i].val, function(d) { return d.total; })
+  				]);
+
+  			d3.select("#axisYFall17")
+  				.transition().duration(500)
+  				.call(d3.axisLeft(yF17));
+
+  			f17BarChart.data(fall17ByClass[i].val).transition().duration(500)
+  				.attr("x", function(d) { return x(d.week); })
+  				.attr("width", x.bandwidth())
+  				.attr("y", function(d) { return yF17(d.total); })
+  				.attr("height", function(d) { return height - yF17(d.total); });
+  		});
+
+  	d3.select(".classF17Rect").style("fill", "steelblue");
+  	d3.select(".classF17Text").style("stroke", "#fff");
+
+  	////////////////////////////////////////////////////
+
+  	yS18.domain([
+		d3.min(spring18ByClass[0].val, function(d) { return d.total; }),
+		d3.max(spring18ByClass[0].val, function(d) { return d.total; })
+		]);
+
+  	chartS18Graphic.append("g")
+  		.attr("class", "axisX")
+  		.attr("transform", "translate(0," + height + ")")
+  		.call(d3.axisBottom(x).ticks(17))
+  	.append("text")
+  		.attr("transform", "translate(" + width/2 + ", 30)")
+  		.attr("y", 8)
+  		.attr("dy", "0.71em")
+  		.attr("fill", "#000")
+  		.attr("text-align", "right")
+  		.style("font-size", "30px")
+  		.text("Week");
+
+  	chartS18Graphic.append("g")
+  		.attr("class", "axisY")
+  		.attr("id", "axisYSpring18")
+  		.call(d3.axisLeft(yS18))
+  	.append("text")
+  		.attr("transform", "rotate(-90)")
+  		.attr("y", 0 - margin.left + 30)
+  		.attr("x", 0 - height / 2 + 100)
+  		.attr("dy", "0.71em")
+  		.attr("fill", "#000")
+  		.style("font-size", "30px")
+  		.text("Working hours");
+
+  	var s18BarChart = chartS18Graphic.selectAll(".barS18")
+  		.data(spring18ByClass[0].val)
+  		.enter().append("rect")
+  		.attr("class", "barS18")
+  		.attr("x", function(d) { return x(d.week); })
+  		.attr("width", x.bandwidth())
+  		.attr("y", function(d) { return yS18(d.total); })
+  		.attr("height", function(d) { return height - yS18(d.total); });
+
+  	var legendSpring18Graphic = d3.select(".legendSpr18")
+  		.append("g")
+  		.selectAll(".classSpr18")
+  		.data(spring18ByClass)
+  		.enter().append("g")
+  		.attr("class", "classSpr18");
+
+  	var classSpr18Rect = legendSpring18Graphic.append("rect")
+  		.attr("class", "classS18Rect")
+  		.attr("id", function(d) { return "classS18Rect" + d.key.replace(/\s/g, ''); })
+  		.attr("x", function(d, i) {
+  			if(i%2 == 0)
+  				return 0;
+  			else
+  				return 75;
+  		})
+  		.attr("y", function(d, i) {
+  			if(i%2 == 0)
+  				return i / 2 * 35;
+  			else
+  				return (i - 1) / 2 * 35;
+  		})
+  		.attr("width", 70)
+  		.attr("height", 30)
+  		.attr("fill", "#d9d9d9")
+  		.on("click", function(d, i) {
+  			classSpr18Rect.style("fill", "#d9d9d9");
+  			classSpr18Text.style("stroke", "#000");
+  			d3.select(this).style("fill", "#dd1c77");
+  			classSpr18Text._groups[0][i].style.stroke = "#fff";
+
+  			yS18.domain([
+  				d3.min(spring18ByClass[i].val, function(d) { return d.total; }),
+				d3.max(spring18ByClass[i].val, function(d) { return d.total; })
+  				]);
+
+  			d3.select("#axisYSpring18")
+  				.transition().duration(500)
+  				.call(d3.axisLeft(yS18));
+
+  			s18BarChart.data(spring18ByClass[i].val).transition().duration(500)
+  				.attr("x", function(d) { return x(d.week); })
+  				.attr("width", x.bandwidth())
+  				.attr("y", function(d) { return yS18(d.total); })
+  				.attr("height", function(d) { return height - yS18(d.total); });
+  		});
+
+  	var classSpr18Text = legendSpring18Graphic.append("text")
+  		.text(function(d) { return d.key; })
+  		.attr("class", "classS18Text")
+  		.attr("x", function(d, i) {
+  			if(i%2 == 0)
+  				return 10;
+  			else
+  				return 85;
+  		})
+  		.attr("y", function(d, i) {
+  			if(i%2 == 0)
+  				return i / 2 * 35 + 10;
+  			else
+  				return (i - 1) / 2 * 35 + 10;
+  		})
+  		.attr("dy", "0.6em")
+  		.style("font", "lighter 15px sans-serif")
+  		.style("stroke", "#000")
+  		.on("click", function(d, i) {
+  			classSpr18Rect.style("fill", "#d9d9d9");
+  			classSpr18Text.style("stroke", "#000");
+  			d3.select(this).style("stroke", "#fff");
+  			classSpr18Rect._groups[0][i].style.fill = "#dd1c77";
+
+  			yS18.domain([
+  				d3.min(spring18ByClass[i].val, function(d) { return d.total; }),
+				d3.max(spring18ByClass[i].val, function(d) { return d.total; })
+  				]);
+
+  			d3.select("#axisYSpring18")
+  				.transition().duration(500)
+  				.call(d3.axisLeft(yS18));
+
+  			s18BarChart.data(spring18ByClass[i].val).transition().duration(500)
+  				.attr("x", function(d) { return x(d.week); })
+  				.attr("width", x.bandwidth())
+  				.attr("y", function(d) { return yS18(d.total); })
+  				.attr("height", function(d) { return height - yS18(d.total); });
+  		});
+
+  	d3.select(".classS18Rect").style("fill", "#dd1c77");
+  	d3.select(".classS18Text").style("stroke", "#fff");
 }
 
 function type(d) {
