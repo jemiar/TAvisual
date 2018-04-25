4950c0bc41204d44e11adafd7fa708901bc81f0a
HoangMinh45 <hhuynh20@uic.edu>
2018-04-24 21:46:56 -0500

    add semester field to csv file and new project for dashboard

---
+++ dashboard.js
@@ -0,0 +1,41 @@
+// var test = d3.selectAll("input[type=radio][name=chartType");
+// console.log(test);
+// test.on("change", function() {
+// 	console.log(this.value);
+// });
+
+//Array used for week numbering
+var weekNo = [
+	1, 2, 3, 4,
+	5, 6, 7, 8,
+	9, 10, 11, 12,
+	13, 14, 15, 16, 17
+];
+
+//store raw data after reading csv files
+var rawDataFall17, rawDataSpring18;
+
+//Read data from 2 files, then use await() when finish reading and call assignData function
+//callback function type() used for convert string to number
+d3.queue()
+	.defer(d3.csv, "https://raw.githubusercontent.com/jemiar/TAvisual/master/fall17data.csv", type)
+	.defer(d3.csv, "https://raw.githubusercontent.com/jemiar/TAvisual/master/spring18data.csv", type)
+	.await(assignData);
+
+//type function: convert string to number
+function type(d) {
+  	d.labtime = +d.labtime;
+  	d.classtime = +d.classtime;
+  	d.grading = +d.grading;
+  	d.officehr = +d.officehr;
+  	d.classprep = +d.classprep;
+  	d.other = +d.other;
+  	d.week = +d.week;
+  	return d;
+}
+
+function assignData(error, dataFall17, dataSpring18) {
+	if(error) throw error;
+	rawDataFall17 = dataFall17;
+	rawDataSpring18 = dataSpring18;
+}
